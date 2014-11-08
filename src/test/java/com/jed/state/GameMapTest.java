9f71bbd5ed7edab84549a3da1a49c84bddcbc38c
Peter Colapietro <petercolapietro@gmail.com>
2014-11-08 17:29:27 -0600

    Starting to add Unit Test for #6

---
+++ src/test/java/com/jed/state/GameMapTest.java
@@ -0,0 +1,55 @@
+package com.jed.state;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameMapTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testEntered() throws Exception {
+
+    }
+
+    @Test
+    public void testLeaving() throws Exception {
+
+    }
+
+    @Test
+    public void testKeyPress() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testDraw() throws Exception {
+
+    }
+
+    @Test
+    public void testDrawChildVertex2f() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTileSetPath() throws Exception {
+
+    }
+}
