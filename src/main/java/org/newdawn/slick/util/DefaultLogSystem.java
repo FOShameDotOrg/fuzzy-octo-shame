cccac40512b3156566876110d109ae8f06e07416
Peter Colapietro <petercolapietro@gmail.com>
2014-11-10 12:22:19 -0500

    Removed sysout-over-slf4j dependency, whose nemesistry was brought about by org.newdawn.slick.util; .DefaultLogSystem
    I changed DefaultLogSystem to use SLF4J, however it needs to get reworked. Possibly by reverting the change on DefaultLogSystem.java and implementing something like a Slf4jLogSystem class. I will create an issue in GitHub for this, however for now it works...

--- src/main/java/org/newdawn/slick/util/DefaultLogSystem.java
+++ src/main/java/org/newdawn/slick/util/DefaultLogSystem.java
@@ -1,82 +1,87 @@
 package org.newdawn.slick.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.PrintStream;
 import java.util.Date;
 
 /**
  * The default implementation that just spits the messages out to stdout
- * 
- * @author kevin
+ *
+ * @author kevin, Peter Colapietro
+ *
+ * Updated on 2014-11-10 to use org.slf4j.Logger instead of java.io.PrintStream
  */
 public class DefaultLogSystem implements LogSystem {
-	/** The output stream for dumping the log out on */
-	public static PrintStream out = System.out;
+    /**
+     * The Logger for dumping the log out on
+     */
+    public static final Logger out = LoggerFactory.getLogger(DefaultLogSystem.class);
 
-	/**
-	 * Log an error
-	 * 
-	 * @param message The message describing the error
-	 * @param e The exception causing the error
-	 */
-	public void error(String message, Throwable e) {
-		error(message);
-		error(e);
-	}
+    /**
+     * Log an error
+     *
+     * @param message The message describing the error
+     * @param e       The exception causing the error
+     */
+    public void error(String message, Throwable e) {
+        error(message);
+        error(e);
+    }
 
-	/**
-	 * Log an error
-	 * 
-	 * @param e The exception causing the error
-	 */
-	public void error(Throwable e) {
-		out.println(new Date()+" ERROR:" +e.getMessage());
-		e.printStackTrace(out);
-	}
+    /**
+     * Log an error
+     *
+     * @param e The exception causing the error
+     */
+    public void error(Throwable e) {
+        out.error(new Date() + " ERROR:" + e.getMessage());
+    }
 
-	/**
-	 * Log an error
-	 * 
-	 * @param message The message describing the error
-	 */
-	public void error(String message) {
-		out.println(new Date()+" ERROR:" +message);
-	}
+    /**
+     * Log an error
+     *
+     * @param message The message describing the error
+     */
+    public void error(String message) {
+        out.error(new Date() + " ERROR:" + message);
+    }
 
-	/**
-	 * Log a warning
-	 * 
-	 * @param message The message describing the warning
-	 */
-	public void warn(String message) {
-		out.println(new Date()+" WARN:" +message);
-	}
+    /**
+     * Log a warning
+     *
+     * @param message The message describing the warning
+     */
+    public void warn(String message) {
+        out.warn(new Date() + " WARN:" + message);
+    }
 
-	/**
-	 * Log an information message
-	 * 
-	 * @param message The message describing the infomation
-	 */
-	public  void info(String message) {
-		out.println(new Date()+" INFO:" +message);
-	}
+    /**
+     * Log an information message
+     *
+     * @param message The message describing the infomation
+     */
+    public void info(String message) {
+        out.info(new Date() + " INFO:" + message);
+    }
 
-	/**
-	 * Log a debug message
-	 * 
-	 * @param message The message describing the debug
-	 */
-	public void debug(String message) {
-		out.println(new Date()+" DEBUG:" +message);
-	}
+    /**
+     * Log a debug message
+     *
+     * @param message The message describing the debug
+     */
+    public void debug(String message) {
+        out.debug(new Date() + " DEBUG:" + message);
+    }
 
-	/**
-	 * Log a warning with an exception that caused it
-	 * 
-	 * @param message The message describing the warning
-	 * @param e The cause of the warning
-	 */
-	public void warn(String message, Throwable e) {
-		warn(message);
-		e.printStackTrace(out);
-	}
+    /**
+     * Log a warning with an exception that caused it
+     *
+     * @param message The message describing the warning
+     * @param e       The cause of the warning
+     */
+    public void warn(String message, Throwable e) {
+        warn(message);
+    }
 }
