2f6b1977bc283dc3a89b100c3f5bffefb990317a
Peter Colapietro <petercolapietro@gmail.com>
2014-11-23 21:08:05 -0500

    Removed unused import.

--- src/main/java/org/newdawn/slick/Input.java
+++ src/main/java/org/newdawn/slick/Input.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.Iterator;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Controller;
