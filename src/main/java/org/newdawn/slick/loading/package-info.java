a8d022c9276b9355d150e97559cd1d72efcebcde
Peter Colapietro <petercolapietro@gmail.com>
2014-11-24 03:41:32 -0500

    Show me your package...
    #Phrasing

---
+++ src/main/java/org/newdawn/slick/loading/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Peter Colapietro on 11/24/14.
+ * @author Peter Colapietro
+ * @since 0.1.5
+ */
+package org.newdawn.slick.loading;
--- src/main/java/org/newdawn/slick/util/package-info.java
+++ src/main/java/org/newdawn/slick/util/package-info.java
@@ -1,4 +1,5 @@
 /**
- *
+ * @author Peter Colapietro
+ * @since 0.1.5
  */
 package org.newdawn.slick.util;
