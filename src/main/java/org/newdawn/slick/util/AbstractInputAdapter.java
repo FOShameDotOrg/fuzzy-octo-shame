3eac760e4b124e8fe6437ae0822051c51dc6ffa2
Peter Colapietro <petercolapietro@gmail.com>
2014-11-24 03:31:47 -0500

    Removed useless semicolon.

--- src/main/java/org/newdawn/slick/util/AbstractInputAdapter.java
+++ src/main/java/org/newdawn/slick/util/AbstractInputAdapter.java
@@ -16,66 +16,67 @@ public abstract class AbstractInputAdapter implements InputListener {
      * @see org.newdawn.slick.InputListener#controllerButtonPressed(int, int)
      */
     @Override
-    public void controllerButtonPressed(int controller, int button){};
+    public void controllerButtonPressed(int controller, int button){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerButtonReleased(int, int)
      */
     @Override
-    public void controllerButtonReleased(int controller, int button){};
+    public void controllerButtonReleased(int controller, int button){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerDownPressed(int)
      */
     @Override
-    public void controllerDownPressed(int controller){};
+    public void controllerDownPressed(int controller){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerDownReleased(int)
      */
     @Override
-    public void controllerDownReleased(int controller){};
+    public void controllerDownReleased(int controller){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerLeftPressed(int)
      */
     @Override
-    public void controllerLeftPressed(int controller){};
+    public void controllerLeftPressed(int controller){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerLeftReleased(int)
      */
     @Override
-    public void controllerLeftReleased(int controller){};
+    public void controllerLeftReleased(int controller){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerRightPressed(int)
      */
     @Override
-    public void controllerRightPressed(int controller){};
+    public void controllerRightPressed(int controller){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerRightReleased(int)
      */
     @Override
-    public void controllerRightReleased(int controller){};
+    public void controllerRightReleased(int controller){}
+
     /**
      * @see org.newdawn.slick.InputListener#controllerUpPressed(int)
      */
     @Override
-    public void controllerUpPressed(int controller){};
+    public void controllerUpPressed(int controller){}
 
     /**
      * @see org.newdawn.slick.InputListener#controllerUpReleased(int)
      */
     @Override
-    public void controllerUpReleased(int controller){};
+    public void controllerUpReleased(int controller){}
 
     /**
      * @see org.newdawn.slick.InputListener#inputEnded()
      */
     @Override
-    public void inputEnded(){};
+    public void inputEnded(){}
 
     /**
      * @see org.newdawn.slick.InputListener#isAcceptingInput()
@@ -98,60 +99,60 @@ public abstract class AbstractInputAdapter implements InputListener {
      * @see org.newdawn.slick.InputListener#keyPressed(int, char)
      */
     @Override
-    public void keyPressed(int key, char c){};
+    public void keyPressed(int key, char c){}
 
     /**
      * @see org.newdawn.slick.InputListener#keyReleased(int, char)
      */
     @Override
-    public void keyReleased(int key, char c){};
+    public void keyReleased(int key, char c){}
 
     /**
      * @see org.newdawn.slick.InputListener#mouseMoved(int, int, int, int)
      */
     @Override
-    public void mouseMoved(int oldX, int oldY, int newX, int newY){};
+    public void mouseMoved(int oldX, int oldY, int newX, int newY){}
 
     /**
      * @see org.newdawn.slick.InputListener#mousePressed(int, int, int)
      */
     @Override
-    public void mousePressed(int button, int x, int y){};
+    public void mousePressed(int button, int x, int y){}
 
     /**
      * @see org.newdawn.slick.InputListener#mouseReleased(int, int, int)
      */
     @Override
-    public void mouseReleased(int button, int x, int y){};
+    public void mouseReleased(int button, int x, int y){}
 
     /**
      * @see org.newdawn.slick.InputListener#mouseWheelMoved(int)
      */
     @Override
-    public void mouseWheelMoved(int change){};
+    public void mouseWheelMoved(int change){}
 
     /**
      * @see org.newdawn.slick.InputListener#setInput(org.newdawn.slick.Input)
      */
     @Override
-    public void setInput(Input input){};
+    public void setInput(Input input){}
 
     /**
      * @see org.newdawn.slick.InputListener#mouseClicked(int, int, int, int)
      */
     @Override
-    public void mouseClicked(int button, int x, int y, int clickCount){};
+    public void mouseClicked(int button, int x, int y, int clickCount){}
 
 
     /**
      * @see org.newdawn.slick.InputListener#mouseDragged(int, int, int, int)
      */
     @Override
-    public void mouseDragged(int oldX, int oldY, int newX, int newY){};
+    public void mouseDragged(int oldX, int oldY, int newX, int newY){}
 
     /**
      * @see org.newdawn.slick.ControlledInputReceiver#inputStarted()
      */
     @Override
-    public void inputStarted(){};
+    public void inputStarted(){}
 }
