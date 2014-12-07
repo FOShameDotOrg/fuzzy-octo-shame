2e8052dda0728c169871f48360d8cf21910c72a4
Peter Colapietro <petercolapietro@gmail.com>
2014-12-07 07:35:43 -0500

    Check yo style.

--- src/main/java/org/newdawn/slick/util/LogSystem.java
+++ src/main/java/org/newdawn/slick/util/LogSystem.java
@@ -1,14 +1,14 @@
 package org.newdawn.slick.util;
 
 /**
- * Plugin in interface for the logging of Slick
+ * Plugin in interface for the logging of Slick.
  * 
  * @author kevin
  */
 interface LogSystem {
 
     /**
-     * Log an error
+     * Log an error.
      *
      * @param message The message describing the error
      * @param e The exception causing the error
@@ -16,28 +16,28 @@ interface LogSystem {
     public void error(String message, Throwable e);
 
     /**
-     * Log an error
+     * Log an error.
      *
      * @param e The exception causing the error
      */
     public void error(Throwable e);
 
     /**
-     * Log an error
+     * Log an error.
      *
      * @param message The message describing the error
      */
     public void error(String message);
 
     /**
-     * Log a warning
+     * Log a warning.
      *
      * @param message The message describing the warning
      */
     public void warn(String message);
 
     /**
-     * Log a warning
+     * Log a warning.
      *
      * @param message The message describing the warning
      * @param e The cause of the warning
@@ -45,14 +45,14 @@ interface LogSystem {
     public void warn(String message, Throwable e);
 
     /**
-     * Log an information message
+     * Log an information message.
      *
      * @param message The message describing the infomation
      */
     public void info(String message);
 
     /**
-     * Log a debug message
+     * Log a debug message.
      *
      * @param message The message describing the debug
      */
